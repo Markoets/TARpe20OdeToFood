ec1e3cb6679fbbdc3e27680a58cd6e7a7cde7342
Marko <opilane@HP-CZC6297B90>
2021-12-06 15:38:17 +0200

    Add validation script section

--- OdeToFood/Models/viewModels/ReviewViewModel.cs
+++ OdeToFood/Models/viewModels/ReviewViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,7 +10,10 @@ namespace OdeToFood.Models.viewModels
     {
             
         public int Id { get; set; }
+        [Range(1,10)]
         public int Rating { get; set; }
+        [Required]
+        [StringLength(1024)]
         public string Body { get; set; }
     }
 }
