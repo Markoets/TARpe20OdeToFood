3cb0764c62f8459d08c763115ccf1aaf704e373a
Marko <opilane@HP-CZC6297B90>
2021-12-07 09:50:43 +0200

    Add search form with Ajax

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCore.Unobtrusive.Ajax;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using OdeToFood.Data;
 using OdeToFood.Models;
@@ -50,6 +52,10 @@ namespace OdeToFood.Controllers
                     CountOfReviews = r.Reviews.Count()
                 });
 
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView("_Restaurants", model);
+            }
 
             return View(model);
         }
